3ef0d19c2eda4da89a2c09382753d98f077dc13e
jekiquin <jci.dev.0@gmail.com>
2023-06-18 16:33:17 -0700

    used cache query to disable caching on nextjs image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,15 @@
-'use client';
 import Img from '@/components/common/img';
-import { useDicebearContext } from '@/context/dicebearContext';
-import { getImage } from '@/fetcher/imageQuery';
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 export default function Home() {
-  const [imgUrl, setImgUrl] = useState('');
-
-  const fetchImageUrl = async () => {
-    const { url } = await getImage(1);
-    setImgUrl(url);
-  };
-
-  useEffect(() => {
-    fetchImageUrl();
-  }, []);
-
-  console.log({ imgUrl });
-
   return (
     <div className={styles.root}>
-      {imgUrl && (
-        <>
-          <h2>Avatar</h2>
-          <Img
-            src={`${imgUrl}?cache=${Date.now()}`}
-            alt="saved image"
-            className={styles.image}
-          />
-        </>
-      )}
+      <h2>Avatar</h2>
+      <Img
+        src={`/s3/avatar/1.svg?cache=${Date.now()}`}
+        alt="saved image"
+        className={styles.image}
+      />
     </div>
   );
 }
